76e07f6f49d69265273a4265522c95f27804b6f3
rajesh kumar <rajeshit22003@gmail.com>
2024-10-08 14:22:23 +0530

    final submit

--- program1.js
+++ program1.js
@@ -3,7 +3,26 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    let stack = []; 
+
+    for (let char of s) {
+        
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        } 
+        
+        else {
+            let top = stack.pop();
+            if ((char === ')' && top !== '(') ||
+                (char === '}' && top !== '{') ||
+                (char === ']' && top !== '[')) {
+                return false;
+            }
+        }
+    }
+
     
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
